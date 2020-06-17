61cd2e80e9a6f0ecf76dadbd81eff48cec51cbbe
Devesh044 <67075696+Devesh044@users.noreply.github.com>
2020-06-18 04:39:55 +0530

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$r4!d@r%-*8hk(fivruvr)b1+j*4&xl^g%!d31xv00)*&ri-(^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://devesh044.pythonanywhere.com/']
 
 
 # Application definition
